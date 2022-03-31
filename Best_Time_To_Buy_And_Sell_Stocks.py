5f0f53052219ae69b4ffa525ab8aa443243aaa3b
Anshu Saxena <theashknight16@gmail.com>
2022-03-31 11:58:46 +0530

    Add files via upload

---
+++ Best_Time_To_Buy_And_Sell_Stocks.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, price: List[int]) -> int:
+        mxpro=0
+        for i in range(1,len(price)):
+            if price[i]>price[i-1]:
+                mxpro+=(price[i]-price[i-1])
+        return mxpro
